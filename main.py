fb8dbeecf30c733df6a8d9d8965788a9da985f63
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2021-11-13 17:38:44 -0500

    fetch prices

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from portfolio import Portfolio
 
 
 ptf = Portfolio(account='tfsa')
+ptf.fetch_prices(read=True)
 
-start = datetime(2021, 10, 1)
-end = datetime(2021, 10, 5)
-
-for pos in ptf.positions.keys():
-    ptf.get_position(pos).fetch_prices(start, end, read=False)
-
-pos = ptf.get_position('HTGC')
 print('')
--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from data_sources.alphavantage_connection import AlphaVantageConnection
 from position import Position
 from transaction_manager import TransactionManager
@@ -23,7 +25,11 @@ class Portfolio(object):
         tickers = self.transaction_manager.live_tickers()
         for ticker in tickers:
             self.positions[ticker] = Position(ticker)
-
-
         print(f'positions for {self.account} loaded')
+
+    def fetch_prices(self, read: bool):
+        start = datetime(1900, 1, 1)
+        end = datetime.today()
+        for pos in self.positions.keys():
+            self.get_position(pos).fetch_prices(start, end, read=read)
 
