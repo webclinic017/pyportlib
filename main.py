32afc5769a3d461520cc09a323bef55fa554e5ae
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2021-12-14 20:03:02 -0500

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from datetime import datetime
 from portfolio import Portfolio
-from transaction import Transaction
 import quantstats as qs
 qs.extend_pandas()
 
 
 ptf = Portfolio(account='tfsa', load_data=True)
-ptf.refresh_all()
 print('')
 
 
