d754d91df0c2f18d0fc58ea0cd69e632bf943340
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-01-28 14:13:42 -0500

    logging bug

--- testing.py
+++ testing.py
@@ -1,9 +1,8 @@
 import portofolio as porto
 import logging
 
-logging.getLogger().setLevel(logging.ERROR)
-
 ptf = porto.Portfolio(account='tfsa', currency="CAD")
+print(ptf.transactions.head())
 
 aapl = porto.Position(ticker='AAPL', currency='USD')
 
