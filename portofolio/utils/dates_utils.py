455fd2356a51584a401ddfdacc1f7736fc88d7ae
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-01-31 13:53:06 -0500

    start date bug

--- portofolio/utils/dates_utils.py
+++ portofolio/utils/dates_utils.py
@@ -10,7 +10,10 @@ def get_market_days(start: datetime, end: datetime = None, market: str = None) -
     if end is None:
         end = datetime.today()
     # TSX is TRT
-    index = mcal.date_range(mcal.get_calendar(market).schedule(start_date=start, end_date=end), frequency='1D')
-    index = index.tz_convert(None)
-    index = [i.replace(hour=0, minute=0, second=0, microsecond=0) for i in index]
+    try:
+        index = mcal.date_range(mcal.get_calendar(market).schedule(start_date=start, end_date=end), frequency='1D')
+        index = index.tz_convert(None)
+        index = [i.replace(hour=0, minute=0, second=0, microsecond=0) for i in index]
+    except ValueError:
+        index = []
     return index
--- testing.py
+++ testing.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 import portofolio as porto
 
-ptf = porto.Portfolio(account='tfsa', currency="CAD")
-ptf.update_data()
+ptf = porto.Portfolio(account='Example', currency="CAD")
+# ptf.update_data()
 
 pnl = ptf.pct_daily_total_pnl(start_date=ptf.start_date).iloc[1:]
 porto.reporting.full_html(pnl, "SPY", name=f"{ptf.account}_{datetime.today().date()}", rf=0.)
