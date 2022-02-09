f0d5c6421f9bca00f7f05839c5e84536aed733c0
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-02-08 19:06:08 -0500

    reporting now takes in directly the ptf

--- portofolio/reporting/reporting.py
+++ portofolio/reporting/reporting.py
@@ -3,6 +3,7 @@ from typing import Union
 import pandas as pd
 import quantstats as qs
 
+import portofolio
 from portofolio.data_sources.data_reader import DataReader
 from portofolio.utils import files_utils
 
@@ -11,15 +12,20 @@ if not files_utils.check_dir(OUT_DIR):
     files_utils.make_dir(OUT_DIR)
 
 
-def full_html(strategy_returns: pd.Series, benchmark: Union[pd.Series, str], name: str, rf=None) -> None:
+# TODO add start and end date with defaults
+def full_html(portfolio: portofolio.Portfolio, benchmark: Union[pd.Series, str], name: str, rf=None) -> None:
     """
     produces quantstats html report
     :param rf: riskfree rate
-    :param strategy_returns: strategy returns in %
+    :param portfolio: portfolio object
     :param benchmark: benchmark returns in % or ticker of benchmark
     :param name: name of saved file
     :return: None
     """
+
+    portfolio.update_data()
+    strategy_returns = portfolio.pct_daily_total_pnl(start_date=portfolio.start_date)
+
     if rf is None:
         rf = 0.
     if isinstance(benchmark, str):
--- testing.py
+++ testing.py
@@ -2,9 +2,6 @@ from datetime import datetime
 import portofolio as porto
 
 ptf = porto.Portfolio(account='tfsa', currency="CAD")
-# ptf.update_data()
 
-pnl = ptf.pct_daily_total_pnl(start_date=ptf.start_date)
 
-pnl = pnl.iloc[1:]
-porto.reporting.full_html(pnl, "SPY", name=f"{ptf.account}_{datetime.today().date()}", rf=0.)
+porto.reporting.full_html(ptf, "SPY", name=f"{ptf.account}_{datetime.today().date()}", rf=0.)
