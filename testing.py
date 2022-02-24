8af09e7096c08553261ab72a3030515514133350
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-02-24 18:18:32 -0500

    struct

--- testing.py
+++
@@ -1,9 +0,0 @@
-import portofolio as porto
-
-ticker = "CM.TO"
-pos = porto.Position(ticker, local_currency="CAD")
-pos.update_prices()
-
-
-print(pos.prices.iloc[-1])
-print(pos.quantities.iloc[-1])
