0d96afa547b26c931d0cf6df307cf815cf7d9aa4
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-02-25 14:45:49 -0500

    pub

--- portofolio/__init__.py
+++ portofolio/__init__.py
@@ -2,4 +2,3 @@ from .portfolio import Portfolio
 from .position import Position
 from .reporting import reporting
 from .helpers.transaction import Transaction
-from .utils import config_utils
--- tfsa_report.py
+++ tfsa_report.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import portofolio.src as porto
+import portofolio as porto
 
 ptf = porto.Portfolio(account='tfsa', currency="CAD")
 ptf.update_data()
