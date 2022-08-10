0fd811e1075ae759f2b91eec5c78719f77c9ee81
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2022-08-10 19:43:24 -0400

    private variables in create.py

--- pyportlib/create.py
+++ pyportlib/create.py
@@ -7,43 +7,42 @@ from pyportlib.containers.position_container import PositionContainer
 from pyportlib.position.iposition import IPosition
 from pyportlib.utils import config_utils
 
-data_source_config = config_utils.data_source_config()
+_data_source_config = config_utils.data_source_config()
 
-ptf_container = PortfolioContainer()
-position_container = PositionContainer()
-services_container = ServicesContainer()
-datareader_container = DataReaderContainer(config=data_source_config)
+_ptf_container = PortfolioContainer()
+_position_container = PositionContainer()
+_services_container = ServicesContainer()
+_datareader_container = DataReaderContainer(config=_data_source_config)
 
 
 def portfolio(account: str, currency: str):
-    datareader = datareader_container.datareader()
+    datareader = _datareader_container.datareader()
 
-    cash_manager = services_container.cash_manager(account=account)
-    transaction_manager = services_container.transaction_manager(account=account)
+    cash_manager = _services_container.cash_manager(account=account)
+    transaction_manager = _services_container.transaction_manager(account=account)
 
     required_currencies = transaction_manager.get_currencies()
+    fx = _services_container.fx(ptf_currency=currency, currencies=required_currencies, datareader=datareader)
 
-    fx = services_container.fx(ptf_currency=currency, currencies=required_currencies, datareader=datareader)
-
-    ptf = ptf_container.ptf(account=account,
-                            currency=currency,
-                            cash_manager=cash_manager,
-                            transaction_manager=transaction_manager,
-                            fx=fx,
-                            datareader=datareader)
+    ptf = _ptf_container.ptf(account=account,
+                             currency=currency,
+                             cash_manager=cash_manager,
+                             transaction_manager=transaction_manager,
+                             fx=fx,
+                             datareader=datareader)
 
     return ptf
 
 
 def position(ticker: str, local_currency: str = None, tag: str = None) -> IPosition:
-    datareader = datareader_container.datareader()
+    datareader = _datareader_container.datareader()
 
-    position = position_container.position(ticker=ticker,
-                                           local_currency=local_currency,
-                                           tag=tag,
-                                           datareader=datareader)
+    pos = _position_container.position(ticker=ticker,
+                                            local_currency=local_currency,
+                                            tag=tag,
+                                            datareader=datareader)
 
-    return position
+    return pos
 
 
 def transaction(date: datetime,
@@ -53,19 +52,19 @@ def transaction(date: datetime,
                 price: float,
                 fees: float,
                 currency: str):
-    trx = services_container.transaction(date=date,
-                                         ticker=ticker,
-                                         transaction_type=transaction_type,
-                                         quantity=quantity,
-                                         price=price,
-                                         fees=fees,
-                                         currency=currency)
+    trx = _services_container.transaction(date=date,
+                                          ticker=ticker,
+                                          transaction_type=transaction_type,
+                                          quantity=quantity,
+                                          price=price,
+                                          fees=fees,
+                                          currency=currency)
 
     return trx
 
 
 def cash_change(date: datetime, direction: str, amount: float):
-    cc = services_container.cash_change(date=date,
-                                        direction=direction,
-                                        amount=amount)
+    cc = _services_container.cash_change(date=date,
+                                         direction=direction,
+                                         amount=amount)
     return cc
